860282140ef5a7c075ba094288ae92a0ff26ad07
Chris Maguire <cwmaguire@protonmail.com>
2023-11-11 00:48:42 -0800

    Remove commented-out code, unused file
    Now that I've got this in git I don't need to save my commented-out
    code.
    
    I'm using stat for file size, so I don't need to get it from the EXIF
    information.
    
    Updated the TODOs

--- photo_org.sh
+++ photo_org.sh
@@ -8,18 +8,11 @@
 # Name photos with date and camera model
 #
 # TODO:
-# - see if you can use different fields for model:
-#   - "Software"
-#     - e.g. "Picasa"
-#   - "xmp:CreatorTool"
-#   - "photoshop:DateCreated"     <-- use for date
-#   - UserComment
-#     - e.g. Screenshot (screenshot shows that it's from a phone)
 #   - maybe keep screenshots somewhere else, or throw them away
 #     - might be from Google Hangouts
 #
-#   Seems like a lot of the "no model" "photos" are actually screenshots.
-#   I should maybe put these in a different directory structure
+#   - Seems like a lot of the "no model" "photos" are actually screenshots.
+#     I should maybe put these in a different directory structure
 #
 # - get dates for PNG/JPG files from the accompanying JSON file
 #   - e.g. /home/c/4tb/pics/goog_pics_2022/pics_2022/2022_chris_iphone_pics/Takeout/Google Photos/Photos from 2021/
@@ -55,16 +48,7 @@ function main {
 
   local file_size=$(stat --format=%s "${photo_path}")
 
-  #echo "call exif_fields ${photo_path}"
   exif_fields "${photo_path}"
-
-  #echo "out_idr = ${out_dir}"
-  #echo "photo_path = ${photo_path}"
-  #echo "photo_file = ${photo_file}"
-  #echo "ext = ${ext}"
-
-  #exif=$(exiftool -csv -CreateDate -Model -FileNumber -SequenceNumber "${photo_path}" | tail -1)
-  #echo "exif = ${exif}"
 
   photo_date=$(echo ${create_date} | awk -F'[: -]' '{print $1 "_" $2 "_" $3 "_" $4 "_" $5 "_" $6}')
   photo_year=${photo_date%%_*}
@@ -76,14 +60,11 @@ function main {
   maybe_seq=${sequence_number}${sequence_number:+_}
   photo_name="${photo_date}_${maybe_model}${maybe_num}${maybe_seq}${file_size}"
 
-  #echo "photo_name is ${photo_name}"
-
   # I'm keeping the extension off in case I want to disambiguate dupes with numbers,
   # e.g. _2, _3, _4
   out_path="${photo_dir}/${photo_name}"
 
   if [[ ! -d ${photo_dir} ]]; then
-    #echo "Creating ${photo_dir}"
     mkdir -p ${photo_dir}
   fi
 
@@ -109,15 +90,12 @@ function exif_fields {
   model ${photo_path}
   file_number ${photo_path}
   sequence_number ${photo_path}
-  #file_size ${photo_path}
 }
 
 function exif_field {
   declare -n var=$1
   exif_field_name=$2
   path=$3
-
-  #echo "Get exif field $1 for file $2"
   var=$(exiftool -csv -${exif_field_name} "${path}" | tail -1 | awk -F, '{print $2}') # | sed -e 's/ /_/g'
 }
 
--- photo_org_file_size.sh
+++
@@ -1,4 +0,0 @@
-function file_size {
-  local photo_path=${1}
-  exif_field file_size FileSize "${photo_path}"
-}
